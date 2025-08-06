b72307ae83d58b465ced8b3c3fedb65575f4a65d
VidhiSoni4 <vidhisoni924@gmail.com>
2025-08-07 00:58:38 +0530

    changes before cart

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -9,6 +9,9 @@ import dessert from './dessert.jpg'
 import bakery from './bakery.png'
 import beverages from './beverages.jpg'
 import combos from './combos.jpg'
+import add_icon_white from './add_icon_white.png'
+import add_icon_green from './add_icon_green.png'
+import remove_icon_red from './remove_icon_red.png'
 
 import Starter_1 from './Starter_1.jpg'
 import Starter_2 from './Starter_2.jpg'
@@ -41,12 +44,55 @@ import Sides6 from './Sides6.jpg'
 import Sides7 from './Sides7.jpg'
 import Sides8 from './Sides8.jpg'
 
+import dessert1 from './dessert1.jpg'
+import dessert2 from './dessert2.jpg'
+import dessert3 from './dessert3.jpg'
+import dessert4 from './dessert4.jpg'
+import dessert5 from './dessert5.jpg'
+import dessert6 from './dessert6.jpg'
+import dessert7 from './dessert7.jpg'
+import dessert8 from './dessert8.jpg'
+import dessert9 from './dessert9.jpg'
+import dessert10 from './dessert10.jpg'
+
+import bakery1 from './bakery1.jpg'
+import bakery2 from './bakery2.jpg'
+import bakery3 from './bakery3.jpg'
+import bakery4 from './bakery4.jpg'
+import bakery5 from './bakery5.jpg'
+import bakery6 from './bakery6.jpg'
+import bakery7 from './bakery7.jpg'
+import bakery8 from './bakery8.jpg'
+import bakery9 from './bakery9.jpg'
+import bakery10 from './bakery10.jpg'
+
+import beverage1 from './beverage1.jpg'
+import beverage2 from './beverage2.jpg'
+import beverage3 from './beverage3.jpg'
+import beverage4 from './beverage4.jpg'
+import beverage5 from './beverage5.jpg'
+import beverage6 from './beverage6.jpg'
+import beverage7 from './beverage7.jpg'
+import beverage8 from './beverage8.jpg'
+import beverage9 from './beverage9.jpg'
+
+import combo1 from './combo1.png'
+import combo2 from './combo2.png'
+import combo3 from './combo3.png'
+import combo4 from './combo4.png'
+import combo5 from './combo5.png'
+import combo6 from './combo6.png'
+import combo7 from './combo7.png'
+
 
 export const assets = {
     logo,
     search_icon,
     cart,
-    header_image
+    header_image,
+    add_icon_white,
+    add_icon_green,
+    remove_icon_red
 }
 
 export const menu_list = [
@@ -90,6 +136,14 @@ export const food_list = [
         category: "Starter"
     },
     {
+        _id: "53",
+        name: "Kir Royal",
+        image: beverage5,
+        price: 10,
+        description: "Sparkling wine (often Champagne) mixed with crème de cassis (blackcurrant liqueur)",
+        category: "Beverages"
+    },
+    {
         _id: "2",
         name: "Salade de chèvre chaud",
         image: Starter_2,
@@ -106,20 +160,20 @@ export const food_list = [
         category: "Starter"
     },
     {
-        _id: "4",
-        name: "Tarte aux légumes",
-        image: Starter_4,
-        price: 10,
-        description: "Vegetable tart, often with tomatoes, zucchini, or onions and cheese",
-        category: "Starter"
-    },
-    {
         _id: "5",
         name: "Salade de betteraves",
         image: Starter_5,
         price: 9,
         description: " Beetroot salad, often with walnuts and goat cheese",
         category: "Starter"
+    },
+    {
+        _id: "38",
+        name: "Croquembouche",
+        image: dessert10,
+        price: 20,
+        description: "A tall, elegant tower of cream-filled choux pastry puffs, held together with caramel",
+        category: "Dessert"
     },
     {
         _id: "6",
@@ -138,14 +192,6 @@ export const food_list = [
         category: "Starter"
     },
     {
-        _id: "8",
-        name: "Crudités avec sauce au yaourt",
-        image: Starter_8,
-        price: 8,
-        description: "Fresh vegetable sticks served with a herbed yogurt dip",
-        category: "Starter"
-    },
-    {
         _id: "9",
         name: "Champignons à la provençale",
         image: Starter_9,
@@ -161,14 +207,22 @@ export const food_list = [
         description: "Deviled eggs with a creamy mustard filling",
         category: "Starter"
     },
+    {
+        _id: "21",
+        name: "Aligot",
+        image: Sides1,
+        price: 14,
+        description: "Mashed potatoes blended with melted Tomme cheese, butter, and garlic",
+        category: "Cheese_Sides"
+    },
 
     {
-        _id: "11",
-        name: "Tian de légumes",
-        image: mainCourse1,
-        price: 16,
-        description: "Oven-baked layered vegetables (zucchini, tomato, eggplant) with herbs",
-        category: "mainCourse"
+        _id: "4",
+        name: "Tarte aux légumes",
+        image: Starter_4,
+        price: 10,
+        description: "Vegetable tart, often with tomatoes, zucchini, or onions and cheese",
+        category: "Starter"
     },
     {
         _id: "12",
@@ -187,27 +241,11 @@ export const food_list = [
         category: "mainCourse"
     },
     {
-        _id: "14",
-        name: "Piperade basquaise (sans œufs)",
-        image: mainCourse4,
-        price: 14,
-        description: "Basque pepper-tomato stew with onions and garlic; optionally vegan",
-        category: "mainCourse"
-    },
-    {
         _id: "15",
         name: "Gratin de courgettes",
         image: mainCourse5,
         price: 13,
         description: "Baked zucchini with béchamel sauce and cheese",
-        category: "mainCourse"
-    },
-    {
-        _id: "16",
-        name: "Soufflé au fromage",
-        image: mainCourse6,
-        price: 16,
-        description: "Stuffed cabbage leaves with rice, herbs, and vegetables (no meat)",
         category: "mainCourse"
     },
     {
@@ -227,12 +265,20 @@ export const food_list = [
         category: "mainCourse"
     },
     {
-        _id: "19",
-        name: "Lentilles à la Dijonnaise",
-        image: mainCourse9,
-        price: 14,
-        description: "Dijon-style lentils in a mustard cream sauce with herbs",
-        category: "mainCourse"
+        _id: "27",
+        name: "Carottes Vichy",
+        image: Sides7,
+        price: 7,
+        description: "Glazed carrots simmered with butter, sugar, and Vichy mineral water",
+        category: "Cheese_Sides"
+    },
+        {
+        _id: "29",
+        name: "Crème Brûlée",
+        image: dessert1,
+        price: 7,
+        description: "A creamy vanilla custard topped with a caramelized sugar crust",
+        category: "Dessert"
     },
     {
         _id: "20",
@@ -242,62 +288,358 @@ export const food_list = [
         description: "Root vegetable gratin (parsnip, carrot, potato) with cheese or béchamel",
         category: "mainCourse"
     },
-    
     {
-        _id: "21",
-        name: "Aligot",
-        image: Sides1,
-        price: 14,
-        description: "Mashed potatoes blended with melted Tomme cheese, butter, and garlic",
-        category: "Cheese_Sides"
-    },{
         _id: "22",
         name: "Fromage de chèvre rôti au miel",
         image: Sides2,
         price: 13,
         description: "Roasted goat cheese with honey and herbs, served with salad or bread",
         category: "Cheese_Sides"
-    },{
-        _id: "23",
-        name: "Céleri rémoulade",
-        image: Sides3,
-        price: 7,
-        description: "	Baked vegetable custard (often carrot, spinach, or zucchini)",
-        category: "Cheese_Sides"
-    },{
+    },
+    {
+        _id: "36",
+        name: "Clafoutis	",
+        image: dessert8,
+        price: 6,
+        description: "Baked flan-like dessert with cherries (or seasonal fruit) in a custard base",
+        category: "Dessert"
+    },
+    {
         _id: "24",
         name: "Flan de légumes",
         image: Sides4,
         price: 9,
         description: "Root vegetable gratin (parsnip, carrot, potato) with cheese or béchamel",
         category: "Cheese_Sides"
-    },{
+    },
+    {
+        _id: "31",
+        name: "Tarte Tatin",
+        image: dessert3,
+        price: 6,
+        description: "An upside-down caramelized apple tart with a buttery crust",
+        category: "Dessert"
+    },
+    {
         _id: "25",
         name: "Confit d’oignons",
         image: Sides5,
         price: 6,
         description: "Sweet and tangy onion jam, often served alongside cheese or bread",
         category: "Cheese_Sides"
-    },{
+    },
+    {
+        _id: "62",
+        name: "Croque Végétarien Combo",
+        image: combo5,
+        price: 11,
+        description: "Croque aux Légumes (grilled sandwich with cheese & veggies) + Limonade Artisanale + Fries",
+        category: "Combo"
+    },
+    {
+        _id: "40",
+        name: "Pain au Chocolat",
+        image: bakery2,
+        price: 3,
+        description: "A croissant-like pastry filled with dark chocolate sticks",
+        category: "Bakery_breakfast"
+    },
+    {
         _id: "26",
         name: "Polenta crémeuse au fromage",
         image: Sides6,
         price: 9,
         description: "Creamy cornmeal with cheese and butter, sometimes served as a base for mains",
         category: "Cheese_Sides"
-    },{
-        _id: "27",
-        name: "Carottes Vichy",
-        image: Sides7,
+    },
+    {
+        _id: "14",
+        name: "Piperade basquaise (sans œufs)",
+        image: mainCourse4,
+        price: 14,
+        description: "Basque pepper-tomato stew with onions and garlic; optionally vegan",
+        category: "mainCourse"
+    }, {
+        _id: "23",
+        name: "Céleri rémoulade",
+        image: Sides3,
         price: 7,
-        description: "Glazed carrots simmered with butter, sugar, and Vichy mineral water",
+        description: "	Baked vegetable custard (often carrot, spinach, or zucchini)",
         category: "Cheese_Sides"
-    },{
+    },
+    {
+        _id: "19",
+        name: "Lentilles à la Dijonnaise",
+        image: mainCourse9,
+        price: 14,
+        description: "Dijon-style lentils in a mustard cream sauce with herbs",
+        category: "mainCourse"
+    },
+    {
+        _id: "33",
+        name: "Mille-Feuille",
+        image: dessert5,
+        price: 6,
+        description: "Layers of puff pastry with vanilla cream filling and icing on top",
+        category: "Dessert"
+    },
+    {
+        _id: "48",
+        name: "Pain Perdu",
+        image: bakery10,
+        price: 6,
+        description: "(French Toast) Sweet egg-soaked brioche, pan-fried and served with syrup or fruit",
+        category: "Bakery_breakfast"
+    },
+    {
+        _id: "39",
+        name: "Croissant",
+        image: bakery1,
+        price: 3,
+        description: "Buttery, flaky crescent-shaped pastry made with layered yeast dough",
+        category: "Bakery_breakfast"
+    },
+    {
+        _id: "34",
+        name: "Profiteroles",
+        image: dessert6,
+        price: 7,
+        description: "Cream-filled choux pastry puffs topped with warm chocolate sauce ",
+        category: "Dessert"
+    },
+    {
+        _id: "46",
+        name: "Pain de Campagne",
+        image: bakery8,
+        price: 4,
+        description: "Country-style round sourdough bread, rustic and hearty",
+        category: "Bakery_breakfast"
+    },
+    {
+        _id: "50",
+        name: "Café",
+        image: beverage2,
+        price: 3,
+        description: "Classic small cup of French espresso, strong and rich",
+        category: "Beverages"
+    },
+    {
+        _id: "35",
+        name: "Madeleines",
+        image: dessert7,
+        price: 6,
+        description: "Small shell-shaped sponge cakes with a lemon or vanilla flavor",
+        category: "Dessert"
+    },
+        {
+        _id: "30",
+        name: "Macarons",
+        image: dessert2,
+        price: 8,
+        description: "Colorful almond meringue cookies with ganache or jam filling",
+        category: "Dessert"
+    },
+    {
+        _id: "8",
+        name: "Crudités avec sauce au yaourt",
+        image: Starter_8,
+        price: 8,
+        description: "Fresh vegetable sticks served with a herbed yogurt dip",
+        category: "Starter"
+    },
+
+        {
         _id: "28",
         name: "Raclette",
         image: Sides8,
         price: 12,
         description: "Melted Raclette cheese is scraped over boiled potatoes, pickles, and optionally vegetables or charcuterie",
         category: "Cheese_Sides"
+    },
+    {
+        _id: "42",
+        name: "Brioche",
+        image: bakery4,
+        price: 4,
+        description: "Soft, sweet, buttery bread often shaped as a loaf or bun",
+        category: "Bakery_breakfast"
+    },
+    {
+        _id: "43",
+        name: "Pain aux Raisins",
+        image: bakery5,
+        price: 3,
+        description: "Spiral pastry with custard and raisins, similar to a Danish",
+        category: "Bakery_breakfast"
+    },
+    
+    {
+        _id: "44",
+        name: "Chausson aux Pommes",
+        image: bakery6,
+        price: 3,
+        description: "Puff pastry filled with smooth apple compote (apple turnover)",
+        category: "Bakery_breakfast"
+    },
+    {
+        _id: "45",
+        name: "Fougasse",
+        image: bakery7,
+        price: 3,
+        description: "Leaf-shaped Provençal bread, often with olives, herbs, or cheese",
+        category: "Bakery_breakfast"
+    },
+        {
+        _id: "47",
+        name: "Flûte",
+        image: bakery9,
+        price: 2,
+        description: "A thinner, longer version of the baguette, extra crusty",
+        category: "Bakery_breakfast"
+    },
+    {
+        _id: "49",
+        name: "Diabolo Menthe",
+        image: beverage1,
+        price: 3,
+        description: "Mint syrup mixed with sparkling lemonade, a favorite among French children",
+        category: "Beverages"
+    },
+    {
+        _id: "51",
+        name: "Café au Lait",
+        image: beverage3,
+        price: 3,
+        description: "(Coffee with hot milk, typically served in a bowl or large cup",
+        category: "Beverages"
+    },
+    {
+        _id: "41",
+        name: "Baguette Tradition",
+        image: bakery3,
+        price: 2,
+        description: "Classic long, crusty French bread with a chewy interior",
+        category: "Bakery_breakfast"
+    },
+    {
+        _id: "52",
+        name: "Champagne",
+        image: beverage4,
+        price: 9,
+        description: "Premium sparkling wine from the Champagne region, elegant with fine bubbles",
+        category: "Beverages"
+    },
+    {
+        _id: "54",
+        name: "Vin Rouge",
+        image: beverage6,
+        price: 5,
+        description: "Traditional red wine, from regions like Bordeaux, Burgundy, or Rhône",
+        category: "Beverages"
+    },
+    {
+        _id: "16",
+        name: "Soufflé au fromage",
+        image: mainCourse6,
+        price: 16,
+        description: "Stuffed cabbage leaves with rice, herbs, and vegetables (no meat)",
+        category: "mainCourse"
+    },
+    {
+        _id: "55",
+        name: "Chocolat Chaud",
+        image: beverage7,
+        price: 4,
+        description: "Thick, rich hot chocolate made with dark chocolate and hot milk",
+        category: "Beverages"
+    },
+    {
+        _id: "56",
+        name: "Jus d’Orange",
+        image: beverage8,
+        price: 3,
+        description: "Fresh-squeezed orange juice, classic with French pastries",
+        category: "Beverages"
+    },
+    {
+        _id: "57",
+        name: "Pain Perdu",
+        image: beverage9,
+        price: 3,
+        description: "Jus de Pomme",
+        category: "Beverages"
+    },
+    {
+        _id: "58",
+        name: "Petit Déj Classique",
+        image: combo1,
+        price: 7,
+        description: "Croissant + Café au Lait + Jus d’Orange ",
+        category: "Combo"
+    },
+    {
+        _id: "32",
+        name: "Éclair au Chocolat",
+        image: dessert4,
+        price: 6,
+        description: "A choux pastry filled with chocolate cream and glazed with chocolate icing",
+        category: "Dessert"
+    },
+    {
+        _id: "59",
+        name: "Crêpe Douceur",
+        image: combo2,
+        price: 6,
+        description: "Sweet Crêpe (Nutella or sugar) + Chocolat Chaud",
+        category: "Combo"
+    },
+    {
+        _id: "60",
+        name: "Déjeuner Provençal",
+        image: combo3,
+        price: 9,
+        description: "Ratatouille + Baguette Tradition + Sparkling Water",
+        category: "Combo"
+    },
+    {
+        _id: "61",
+        name: "Fromage & Fruits",
+        image: combo4,
+        price: 8,
+        description: "Cheese Platter (Brie, Camembert, Comté) + Grapes + Baguette",
+        category: "Combo"
+
+    }, 
+    {
+        _id: "11",
+        name: "Tian de légumes",
+        image: mainCourse1,
+        price: 16,
+        description: "Oven-baked layered vegetables (zucchini, tomato, eggplant) with herbs",
+        category: "mainCourse"
+    },
+    {
+        _id: "63",
+        name: "Brunch Parisien Végétarien",
+        image: combo6,
+        price: 11,
+        description: "Quiche aux Légumes + Salade Verte + Thé au Lait",
+        category: "Combo"
+    },
+    {
+        _id: "37",
+        name: "Opéra Cake",
+        image: dessert9,
+        price: 8,
+        description: "Rich layered almond sponge cake with coffee buttercream and chocolate ganache",
+        category: "Dessert"
+    },
+    {
+        _id: "64",
+        name: "Bistro Combo",
+        image: combo7,
+        price: 11,
+        description: "French Onion Soup + Baguette Slices + Red Wine",
+        category: "Combo"
     }
 ]
