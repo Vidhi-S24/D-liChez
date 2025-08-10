4a14ec9d475234673bd8536c9786622729a4d643
VidhiSoni4 <vidhisoni924@gmail.com>
2025-08-10 22:17:15 +0530

    build

--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -2,26 +2,26 @@ import React, { useContext } from 'react'
 import './PlaceOrder.css'
 import { StoreContext } from '../../context/StoreContext'
 const PlaceOrder = () => {
-  const {getTotalCartAmount}=useContext(StoreContext)
+  const { getTotalCartAmount } = useContext(StoreContext)
   return (
     <form className="place-order">
       <div className="place-order-left">
         <p className="title">Delivery Information</p>
         <div className="multi-fields">
-          <input type="text" placeholder='First Name'/>
-          <input type="text" placeholder='Last Name'/>
+          <input type="text" placeholder='First Name' />
+          <input type="text" placeholder='Last Name' />
         </div>
-        <input type="email" placeholder='Email address'/>
-        <input type="text" placeholder='Street'/>
+        <input type="email" placeholder='Email address' />
+        <input type="text" placeholder='Street' />
         <div className="multi-fields">
-          <input type="text" placeholder='City'/>
-          <input type="text" placeholder='State'/>
+          <input type="text" placeholder='City' />
+          <input type="text" placeholder='State' />
         </div>
         <div className="multi-fields">
-          <input type="text" placeholder='Zip Code'/>
-          <input type="text" placeholder='Country'/>
+          <input type="text" placeholder='Zip Code' />
+          <input type="text" placeholder='Country' />
         </div>
-        <input type="number" placeholder='Phone'/>
+        <input type="number" placeholder='Phone' />
       </div>
       <div className="place-order-right">
         <div className="cart-total">
@@ -33,12 +33,12 @@ const PlaceOrder = () => {
           <hr />
           <div className="cart-total-details">
             <p>Delivery Fee</p>
-            <p>€{getTotalCartAmount()===0?0:2}</p>
+            <p>€{getTotalCartAmount() === 0 ? 0 : 2}</p>
           </div>
           <hr />
           <div className="cart-total-details">
             <p>Total</p>
-            <p>€{getTotalCartAmount()===0?0:getTotalCartAmount() + 2}</p>
+            <p>€{getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</p>
           </div>
           <button>Proceed To Payment</button>
         </div>
