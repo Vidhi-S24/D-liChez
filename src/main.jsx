2ff2bd23425cdff224fb6b7a00183f1a784dc7bb
VidhiSoni4 <vidhisoni924@gmail.com>
2025-08-12 23:43:51 +0530

    comment added

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import { StoreContext } from './context/contextProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter basename="/">
+  {/* <BrowserRouter basename="/D-liChez"> for localhost*/}
     <StoreContextProvider>
       <App />
     </StoreContextProvider>
